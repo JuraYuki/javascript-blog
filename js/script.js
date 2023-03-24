715594f9d62f39baa5d1faee72d97bd4667e2b0d
Sylwia Litynska <sylwia.litynska@gmail.com>
2023-03-24 22:18:47 +0100

    add active class

--- js/script.js
+++ js/script.js
@@ -3,16 +3,33 @@
   //  console.log('links:', links);
 //});
 
-const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
+    const titleClickHandler = function(event){
+      event.preventDefault();
+      const clickedElement = this;
+        console.log('Link was clicked!');
+        console.log(event);
 
   
     /* remove class 'active' from all article links  */
   
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+
     /* add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
   
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.post a.active');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
